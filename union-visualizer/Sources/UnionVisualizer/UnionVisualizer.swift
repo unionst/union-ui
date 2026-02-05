be4efd2b6b0724fb215f19f49599c5c3d81afb60
blsage <ben@unionst.com>
2026-02-05 15:00:01 -0800

    Add UIImage initializer and static mode to Visualizer

--- union-visualizer/Sources/UnionVisualizer/UnionVisualizer.swift
+++ union-visualizer/Sources/UnionVisualizer/UnionVisualizer.swift
@@ -7,17 +7,39 @@ public struct Visualizer: View {
     private let image: Image
     private let animationSpeed: Double
     private let blur: CGFloat
+    private let isStatic: Bool
 
     private static let maxImageSize: CGFloat = 100
 
     public init(
         _ resource: ImageResource,
         animationSpeed: Double = 1.0,
-        blur: CGFloat = 20
+        blur: CGFloat = 20,
+        isStatic: Bool = false
     ) {
         self.image = Self.loadDownsampled(resource)
         self.animationSpeed = animationSpeed
         self.blur = blur
+        self.isStatic = isStatic
+    }
+
+    public init(
+        _ uiImage: UIImage,
+        animationSpeed: Double = 1.0,
+        blur: CGFloat = 20,
+        isStatic: Bool = false
+    ) {
+        self.image = Self.loadDownsampled(uiImage)
+        self.animationSpeed = animationSpeed
+        self.blur = blur
+        self.isStatic = isStatic
+    }
+
+    private static func loadDownsampled(_ uiImage: UIImage) -> Image {
+        autoreleasepool {
+            let downsampled = downsample(uiImage, maxSize: maxImageSize)
+            return Image(uiImage: downsampled)
+        }
     }
 
     private static func loadDownsampled(_ resource: ImageResource) -> Image {
@@ -40,58 +62,70 @@ public struct Visualizer: View {
     }
 
     public var body: some View {
+        if isStatic {
+            staticContent
+        } else {
+            animatedContent
+        }
+    }
+
+    private var staticContent: some View {
+        GeometryReader { geometry in
+            let size = geometry.size
+            layeredContent(size: size, time: 0)
+        }
+    }
+
+    private var animatedContent: some View {
         TimelineView(.animation) { timeline in
             GeometryReader { geometry in
                 let size = geometry.size
                 let time = timeline.date.timeIntervalSinceReferenceDate * animationSpeed
-
-                // Background layer animation
-                let rotation0 = Angle(radians: time * 0.01)
-                let offsetX0 = cos(time * 0.02) * size.width * 0.05
-                let offsetY0 = sin(time * 0.015) * size.height * 0.05
-
-                // Top layer animation
-                let rotation1 = Angle(radians: time * 0.02)
-                let offsetX1 = cos(time * 0.03) * size.width * 0.15
-                let offsetY1 = sin(time * 0.025) * size.height * 0.1 - size.height * 0.25
-
-                // Bottom layer animation
-                let rotation2 = Angle(radians: time * -0.015 + 1.0)
-                let offsetX2 = cos(time * 0.035 + 2.0) * size.width * 0.15
-                let offsetY2 = sin(time * 0.03 + 2.0) * size.height * 0.1 + size.height * 0.25
-
-                ZStack {
-                    // Background image
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: size.width * 2.0, height: size.height * 2.0)
-                        .rotationEffect(rotation0)
-                        .offset(x: offsetX0, y: offsetY0)
-
-                    // Top image
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: size.width * 1.0, height: size.height * 0.7)
-                        .rotationEffect(rotation1)
-                        .offset(x: offsetX1, y: offsetY1)
-
-                    // Bottom image
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: size.width * 1.0, height: size.height * 0.7)
-                        .rotationEffect(rotation2)
-                        .offset(x: offsetX2, y: offsetY2)
-                }
-                .saturation(1.3)
-                .contrast(0.4)
-                .brightness(-0.15)
-                .blur(radius: blur)
-                .frame(width: size.width, height: size.height)
-                .clipped()
+                layeredContent(size: size, time: time)
             }
         }
+    }
+
+    private func layeredContent(size: CGSize, time: Double) -> some View {
+        let rotation0 = Angle(radians: time * 0.01)
+        let offsetX0 = cos(time * 0.02) * size.width * 0.05
+        let offsetY0 = sin(time * 0.015) * size.height * 0.05
+
+        let rotation1 = Angle(radians: time * 0.02)
+        let offsetX1 = cos(time * 0.03) * size.width * 0.15
+        let offsetY1 = sin(time * 0.025) * size.height * 0.1 - size.height * 0.25
+
+        let rotation2 = Angle(radians: time * -0.015 + 1.0)
+        let offsetX2 = cos(time * 0.035 + 2.0) * size.width * 0.15
+        let offsetY2 = sin(time * 0.03 + 2.0) * size.height * 0.1 + size.height * 0.25
+
+        return ZStack {
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: size.width * 2.0, height: size.height * 2.0)
+                .rotationEffect(rotation0)
+                .offset(x: offsetX0, y: offsetY0)
+
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: size.width * 1.0, height: size.height * 0.7)
+                .rotationEffect(rotation1)
+                .offset(x: offsetX1, y: offsetY1)
+
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: size.width * 1.0, height: size.height * 0.7)
+                .rotationEffect(rotation2)
+                .offset(x: offsetX2, y: offsetY2)
+        }
+        .saturation(1.3)
+        .contrast(0.4)
+        .brightness(-0.15)
+        .blur(radius: blur)
+        .frame(width: size.width, height: size.height)
+        .clipped()
     }
 }
